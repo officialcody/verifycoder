d40169a3bfdbdc0a73e59d0fe2e55b75144b37d5
Sparsh Goel <sparshgoelt26@gmail.com>
2025-02-13 00:12:04 +0530

    DB: Setup convex and created a users table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,7 @@ import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
+      Home Page
     </div>
   );
 }
