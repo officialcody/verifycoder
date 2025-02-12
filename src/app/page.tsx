11155da0a014c7cd9326c6aefbd70b28e6fdcd91
Sparsh Goel <sparshgoelt26@gmail.com>
2025-02-13 01:19:33 +0530

    Webhook: Added a webhook for creating a user into convex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
-      Home Page
+    <div className="m-10">
+      <SignedOut>
+        <SignInButton>
+          <Button>Login</Button>
+        </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </div>
   );
 }
