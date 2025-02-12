e8aa40ca0a5ed7f5e7f2d476c1047d7461354af1
Sparsh Goel <sparshgoelt26@gmail.com>
2025-02-12 14:14:10 +0530

    Auth: Added Clerk for authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </div>
   );
 }
